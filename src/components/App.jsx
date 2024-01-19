737297718eb86d60eff204d79ceafba3c16897ab
Romeo Alpha <rahul.t2001@gmail.com>
2024-01-19 12:40:13 +0530

    successfully executed task of getting current time using useState hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Heading from './Heading';
 import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-let count = 0;
-
-function increase() {
-  count++;
-  root.render(
-    <div className="container">
-      <h1>{count}</h1>
-      <button onClick={increase}> + </button>
-      <button onClick={decrease}> - </button>
-    </div>
-  );
-}
-
-function decrease() {
-  count--;
-  root.render(
-    <div className="container">
-      <h1>{count}</h1>
-      <button onClick={increase}> + </button>
-      <button onClick={decrease}> - </button>
-    </div>
-  );
-}
+// let count = 0;
 
 function App() {
+  const now = new Date().toLocaleTimeString();
+  const [time, updateTime] = useState(now);
+
+  function showCurrentTime() {
+    const newTime = new Date().toLocaleTimeString();
+    updateTime(newTime);
+    setInterval(showCurrentTime, 1000);
+  }
+
   return (
     <div className="container">
-      <h1>{count}</h1>
-      <button onClick={increase}> + </button>
-      <button onClick={decrease}> - </button>
+      <h1>{time}</h1>
+      <button onClick={showCurrentTime}> Start clock </button>
     </div>
   );
 }
