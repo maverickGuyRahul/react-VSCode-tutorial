3a57d9fe1a2f43b49d433e50ac9f76519e0049c1
Romeo Alpha <rahul.t2001@gmail.com>
2024-02-06 17:15:43 +0530

    6.2.24 - First Half

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,33 +3,37 @@ import React, { useState } from 'react';
 // let count = 0;
 
 function App() {
-  const [headingText, setHeadingText] = useState('Hello');
-  const [isMouseOver, setMouseOver] = useState(false);
+  const [fName, setFName] = useState('');
+  const [lName, setLName] = useState('');
 
-  function handleMyClick() {
-    setHeadingText('Submitted!');
+  function updateFName(event) {
+    let inputName = event.target.value;
+    setFName(inputName);
   }
 
-  function handleMouseOver() {
-    setMouseOver(true);
-  }
-
-  function handleMouseOut() {
-    setMouseOver(false);
+  function updateLName(event) {
+    let inputName = event.target.value;
+    setLName(inputName);
   }
 
   return (
     <div className="container">
-      <h1> {headingText} </h1>
-      <input type="text" placeholder="What is your name?" />
-      <button
-        style={{ backgroundColor: isMouseOver ? 'black' : '#6bbe92' }}
-        onClick={handleMyClick}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-      >
-        Submit
-      </button>
+      <h1>
+        Hello {fName} {lName}
+      </h1>
+      <input
+        type="text"
+        placeholder="What is your first name?"
+        onChange={updateFName}
+        value={fName}
+      />
+      <input
+        type="text"
+        placeholder="What is your last name?"
+        onChange={updateLName}
+        value={lName}
+      />
+      <button>Submit</button>
     </div>
   );
 }
